c895e716736ed027285425cb398cb7f020345d8f
Flavien <flavien.murail@gmail.com>
2020-05-04 15:32:52 +0200

    Minor change in settings #3

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['https://pbflavien.herokuapp.com/']
+ALLOWED_HOSTS = ['https://pbflavien.herokuapp.com']
 
 
 # Application definition
