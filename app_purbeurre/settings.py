8a9387f9f070c7db72a36a604e254a11f3994834
Flavien <flavien.murail@gmail.com>
2020-05-12 12:12:15 +0200

    Try to configure database

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'app_purbeurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_purbeurre',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'USER': 'admin_purbeurre',
+        'PASSWORD': 'purbeurre',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
