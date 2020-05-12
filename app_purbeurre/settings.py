d82b9fda88c0a52e736788e7e0127c5717e8ad73
Flavien <flavien.murail@gmail.com>
2020-05-12 17:14:39 +0200

    Back to previous staticfile path

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 LOGIN_REDIRECT_URL = "/"
