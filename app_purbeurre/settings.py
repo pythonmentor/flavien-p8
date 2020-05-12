8ef78cb9c7575835ce6e53f22fcd59b2d7656ef5
Flavien <flavien.murail@gmail.com>
2020-05-12 17:38:19 +0200

    Adapted the settings file

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -33,7 +33,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['pbflavien.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['purbeurreflavien.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
